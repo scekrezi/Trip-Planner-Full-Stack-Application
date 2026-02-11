de746112ac128c69c82cb465a0cdffacac8f3446
Sindi Cekrezi <s.cekrezi@icloud.com>
2026-02-11 11:23:52 -0600

    add findTripById service and controller

--- server/src/main/java/learn/travelplanner/controllers/TripController.java
+++ server/src/main/java/learn/travelplanner/controllers/TripController.java
@@ -1,11 +1,12 @@
 package learn.travelplanner.controllers;
 
+import learn.travelplanner.domain.Result;
 import learn.travelplanner.domain.TripService;
 import learn.travelplanner.models.Trip;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +23,14 @@ public class TripController {
     @GetMapping("/templates")
     public List<Trip> findTemplates() {
         return service.findTemplates();
+    }
+
+    @GetMapping("/{tripId}")
+    public ResponseEntity<Object> findById(@PathVariable int tripId) throws DataAccessException {
+        Result<Trip> result = service.findById(tripId);
+        if (!result.isSuccess()) {
+            return ErrorResponse.build(result);
+        }
+        return new ResponseEntity<>(result.getPayload(), HttpStatus.OK);
     }
 }
--- server/src/main/java/learn/travelplanner/domain/TripService.java
+++ server/src/main/java/learn/travelplanner/domain/TripService.java
@@ -2,6 +2,7 @@ package learn.travelplanner.domain;
 
 import learn.travelplanner.data.TripsRepository;
 import learn.travelplanner.models.Trip;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,8 +15,20 @@ public class TripService {
         this.repository = repository;
     }
 
-    public List<Trip> findTemplates() {
+    public List<Trip> findTemplates() throws DataAccessException {
         return repository.findTemplates();
+    }
+
+    public Result<Trip> findById(int tripId) throws DataAccessException {
+        Trip found = repository.findById(tripId);
+        Result<Trip> result = new Result<>();
+
+        if (found == null) {
+            result.addErrorMessage("Trip not found", ResultType.NOT_FOUND);
+        } else {
+            result.setPayload(found);
+        }
+        return result;
     }
 
 }
